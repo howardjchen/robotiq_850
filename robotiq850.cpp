7b069940cae9d838d574c8cc12a99930029bcb81
s880367 <s880367@gmail.com>
2017-07-04 21:19:06 +0800

    Complete convert return msg from ASCii to readable
    Exchange open command to close command
    finish TODO 1

--- robotiq850.cpp
+++ robotiq850.cpp
@@ -38,15 +38,16 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-	char cmd[100];
-	char c[100];
+	char Return_data[100];
+	int return_size = 0;
 	boost::asio::io_service robotiq850;
 	boost::asio::serial_port port(robotiq850,"/dev/ttyUSB1");
 	
-	if(port.is_open())
-		printf("port is open\n");
-	else
+	if(!port.is_open())
+	{
+		printf("Cannot open port!!\n");
 		exit(0);
+	}
 
 	boost::asio::serial_port_base::parity::type none;
 	boost::asio::serial_port_base::stop_bits::type one;
@@ -59,46 +60,60 @@ int main(int argc, char const *argv[])
 	unsigned char data[] = {0x09,0x10,0x03,0xE8,0x00,0x03,0x06,0x00,0x00,0x00,0x00,0x00,0x00,0x73,0x30};  
 	port.write_some(boost::asio::buffer(data,sizeof(data)));
 
-	port.read_some (boost::asio::buffer(c,sizeof(c)));
-	for (int i = 0; i < sizeof(c); ++i)
-		printf("%c",c[i]);
-	printf(">>1\n");
+	return_size = port.read_some (boost::asio::buffer(Return_data,sizeof(Return_data)));
+	printf("Response1 : ");
+	for (int i = 0; i < return_size; ++i)
+	{
+		printf("%x",Return_data[i] & 0xff);
+	}
+	printf("\n");
 	usleep(10000);
 
 	// read gripper status until activation is completed
 	unsigned char data2[] = {0x09,0x03,0x07,0xD0,0x00,0x01,0x85,0xCF};  
 	port.write_some(boost::asio::buffer(data2,sizeof(data2)));
 
-	port.read_some (boost::asio::buffer(c,sizeof(c)));
-	for (int i = 0; i < sizeof(c); ++i)
-		printf("%c",c[i]);
-	printf(">>2\n");
+	return_size = port.read_some (boost::asio::buffer(Return_data,sizeof(Return_data)));
+	printf("Response2 : ");
+	for (int i = 0; i < return_size; ++i)
+	{
+		printf("%x",Return_data[i] & 0xff);
+	}
+	printf("\n");
 	sleep(1);
 
 	while(1)
 	{
-		getchar();
 		//Open the Gripper at full speed and full force
-		unsigned char data3[] = {0x09,0x10,0x03,0xE8,0x00,0x03,0x06,0x09,0x00,0x00,0xFF,0xFF,0xFF,0x42,0x29};
+		unsigned char data3[] = {0x09,0x10,0x03,0xE8,0x00,0x03,0x06,0x09,0x00,0x00,0x00,0xFF,0xFF,0x72,0x19};
 		port.write_some(boost::asio::buffer(data3,sizeof(data3)));
 
-		port.read_some (boost::asio::buffer(c,sizeof(c)));
-		for (int i = 0; i < sizeof(c); ++i)
-			printf("%c",c[i]);
+		return_size = port.read_some (boost::asio::buffer(Return_data,sizeof(Return_data)));
+		printf("Open : ");
+		for (int i = 0; i < return_size; ++i)
+		{
+			printf("%x",Return_data[i] & 0xff);
+		}
+		printf("\n");
 
-		printf(">>3\n");
-
-		getchar();
-
+		if(getchar() == 'q')
+			break;
 		//Close the Gripper at full speed and full force
-		unsigned char data4[] = {0x09,0x10,0x03,0xE8,0x00,0x03,0x06,0x09,0x00,0x00,0x00,0xFF,0xFF,0x72,0x19};
+		unsigned char data4[] = {0x09,0x10,0x03,0xE8,0x00,0x03,0x06,0x09,0x00,0x00,0xFF,0xFF,0xFF,0x42,0x29};
 		port.write_some(boost::asio::buffer(data4,sizeof(data4)));
 
-		port.read_some (boost::asio::buffer(c,sizeof(c)));
-		for (int i = 0; i < sizeof(c); ++i)
-			printf("%c",c[i]);
-		printf(">>4\n");
+		return_size = port.read_some (boost::asio::buffer(Return_data,sizeof(Return_data)));
+		printf("Close : ");
+		for (int i = 0; i < return_size; ++i)
+		{
+			printf("%x",Return_data[i] & 0xff);
+		}
+		printf("\n");
+		
+		if(getchar() == 'q')
+			break;
 	}
+
 
 
 	port.close();
